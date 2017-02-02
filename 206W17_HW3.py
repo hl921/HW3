82c0a6f089269c2be58be91d7af592d332a4cfd9
hl921 <haelee@umich.edu>
2017-02-02 10:45:07 -0500

    edit part (c) of part 2

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -57,8 +57,14 @@ for x in r:
 print (file_paths_num)
 
 ## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
+full_paths_num = 0
 
 ## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
+python_course_paths = 0
+for x in r:
+    if "SI206" in x and "py" in x:
+        python_course_paths += 1
+print (python_course_paths)
 
 ## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
 
